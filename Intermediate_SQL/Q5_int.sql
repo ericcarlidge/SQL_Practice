021edc0d19ac64798d48769e2a281cced84d3ed6
ericcarlidge <100529980+ericcarlidge@users.noreply.github.com>
2023-01-18 11:05:36 -0500

    Create Q5_int.sql

---
+++ Intermediate_SQL/Q5_int.sql
@@ -0,0 +1,6 @@
+# Display every patient's first_name.
+# Order the list by the length of each name and then by alphbetically
+
+SELECT first_name
+FROM patients
+ORDER BY LENGTH(first_name), first_name;
