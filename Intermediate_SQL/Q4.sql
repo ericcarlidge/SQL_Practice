19f795c20d28727f966fc9d12ab0ddcca26d3089
ericcarlidge <100529980+ericcarlidge@users.noreply.github.com>
2023-01-18 10:51:23 -0500

    Create Q4.sql

---
+++ Intermediate_SQL/Q4.sql
@@ -0,0 +1,7 @@
+# Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+# Primary diagnosis is stored in the admissions table.
+
+SELECT patients.patient_id, patients.first_name, patients.last_name
+FROM patients
+JOIN admissions ON patients.patient_id = admissions.patient_id
+WHERE diagnosis IS 'Dementia';
