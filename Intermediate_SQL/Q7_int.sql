c61c5e723e630c7699fc8491d59afe4a9fe9b468
ericcarlidge <100529980+ericcarlidge@users.noreply.github.com>
2023-01-18 11:29:01 -0500

    Create Q7_int.sql

---
+++ Intermediate_SQL/Q7_int.sql
@@ -0,0 +1,21 @@
+# Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. 
+# Show results ordered ascending by allergies then by first_name then by last_name.
+
+SELECT first_name, last_name, allergies
+FROM patients
+WHERE allergies = 'Penicillin' OR allergies = 'Morphine'
+ORDER BY allergies ASC, first_name, last_name;
+
+# Alternate
+
+SELECT
+  first_name,
+  last_name,
+  allergies
+FROM patients
+WHERE
+  allergies IN ('Penicillin', 'Morphine')
+ORDER BY
+  allergies,
+  first_name,
+  last_name;
